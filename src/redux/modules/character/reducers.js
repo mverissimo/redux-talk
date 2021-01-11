d65febca26d27f90f0de86cebeef668d9a05d3ea
Matheus Verissimo <mverissimoo@hotmail.com>
2021-01-11 12:04:35 -0300

    feat(redux): add filter reducer

--- src/redux/modules/character/reducers.js
+++ src/redux/modules/character/reducers.js
@@ -27,13 +27,49 @@ export default function reducer(state = initialState, action = {}) {
       };
 
     case Constants.CHARACTER_GET_ALL_SUCCESS:
+      const statusById = {};
+      const idList = {
+        all: [],
+        alive: [],
+        dead: [],
+        unknown: [],
+      };
 
+      action.payload.results.forEach((character) => {
+        statusById[character.id] = character;
+        idList.all.push(character.id);
 
+        if (character.status === "Alive") {
+          idList.alive.push(character.id);
+        }
+
+        if (character.status === "Dead") {
+          idList.dead.push(character.id);
+        }
+
+        if (character.status === "unknown") {
+          idList.unknown.push(character.id);
+        }
+      });
+     
       return {
         ...state,
         list: {
           ...state.list,
           isRequest: false,
+          pages: action.payload.info.pages,
+          hasNext: action.payload.info.next,
+          statusById,
+          idList
+        },
+      };
+
+    case Constants.FILTER_CHARACTERS:
+      return {
+        ...state,
+        list: {
+          ...state.list,
+          status: action.status,
         },
       };
 
